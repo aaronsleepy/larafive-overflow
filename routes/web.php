4f60edfc233ba898b45910d13798470313138be6
aaronsleepy <aaron@kmong.com>
2022-01-31 19:31:26 +0900

    feature: 사용자 관련 라우팅 추가

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,19 @@ Route::get('/', function () {
 });
 
 Route::get('/home', 'HomeController@index');
+
+/* 사용자 가입 과련 ***************************/
+Route::get('/auth/register', [
+    'as' => 'users.create',
+    'uses' => 'UsersController@create'
+]);
+
+Route::post('/auth/register', [
+    'as' => 'users.store',
+    'uses' => 'UsersController@store'
+]);
+
+Route::get('/auth/confirm/{code}', [
+    'as' => 'users.confirm',
+    'uses' => 'UsersController@confirm'
+]);
