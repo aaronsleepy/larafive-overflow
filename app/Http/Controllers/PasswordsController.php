1b9b9945e8aa75e65ab09770d491ad4c3fde3285
aaronsleepy <aaron@kmong.com>
2022-02-01 09:16:56 +0900

    feature: 패스워드 초기화 Form View 조회 API 만들기

--- app/Http/Controllers/PasswordsController.php
+++ app/Http/Controllers/PasswordsController.php
@@ -4,5 +4,12 @@ namespace App\Http\Controllers;
 
 class PasswordsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
 
+    public function getRemind() {
+        return view('passwords.remind');
+    }
 }
