c65610f72f2b2b3f3125ce894345051eb411c9c4
aaronsleepy <aaron@kmong.com>
2022-02-03 19:26:10 +0900

    feature: 패스워드 초기화 신청 설정 구현하기

--- app/Http/Controllers/PasswordsController.php
+++ app/Http/Controllers/PasswordsController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\Console\Output\ConsoleOutput;
+
 class PasswordsController extends Controller
 {
     public function __construct()
@@ -11,5 +16,23 @@ class PasswordsController extends Controller
 
     public function getRemind() {
         return view('passwords.remind');
+    }
+
+    public function postRemind(Request $request)
+    {
+        $this->validate($request, [
+           'email' => 'required|email|exists:users'
+        ]);
+        $email = $request->get('email');
+        $token = str_random(64);
+        DB::table('password_resets')->insert([
+            'email' => $email,
+            'token' => $token,
+            'created_at' => Carbon::now()->toDateTimeString()
+        ]);
+
+        (new ConsoleOutput())->writeln(route('reset.create', $token));
+
+        return redirect('home');
     }
 }
